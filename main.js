63c2960bfb0cae2ea42bf1d90960a552289f45e1
nareshchickenshop <nareshchickenshop@gmail.com>
2025-11-02 17:44:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.getElementById('menu-box');
+  const burger = document.getElementById('burger');
+  menu.classList.toggle('show');
+  burger.classList.toggle('toggle');
+}
